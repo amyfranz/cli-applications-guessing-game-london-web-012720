aff526757dccb53d1b9f95f84e97823ad8e62119
Amy Franz <amy.franz@gmail.com>
2020-01-11 02:16:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,5 +7,6 @@ def run_guessing_game
   elsif user_input == 'exit'
     $stdout.puts("Goodbye!")
   else
-   $stdout.puts("Sorry! The computer guessed #{}.") 
+   $stdout.puts("Sorry! The computer guessed #{random_num}.") 
+ end
 end
