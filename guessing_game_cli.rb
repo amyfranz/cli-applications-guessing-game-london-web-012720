92a04b2f5d5805f320b76a09c97590b7c0fa5fd5
Amy Franz <amy.franz@gmail.com>
2020-01-11 02:20:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   $stdout.puts("please guess a number from 1 - 6")
   user_input = gets
   puts random_num
-  if user_input == random_num.to_s
+  if user_input == random_num
     $stdout.puts("You guessed the correct number!")
   elsif user_input == 'exit'
     $stdout.puts("Goodbye!")
